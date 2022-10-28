f73d4c579f5fae8d3766b2780da9eac87ab17c0c
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-10-28 15:41:59 +0900

    Add 10 espresso tests

--- app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest1.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest1.kt
@@ -29,7 +29,7 @@ import org.junit.Test
 import org.junit.rules.RuleChain
 
 @HiltAndroidTest
-class GardenActivityTest {
+class GardenActivityTest1 {
 
     private val hiltRule = HiltAndroidRule(this)
     private val activityTestRule = ActivityTestRule(GardenActivity::class.java)
