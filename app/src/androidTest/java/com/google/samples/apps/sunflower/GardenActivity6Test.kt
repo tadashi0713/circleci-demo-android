605546944be2d7ec6388332146d4a5356abe7b3b
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2022-11-30 15:38:58 +0900

    space

--- app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivity6Test.kt
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivity6Test.kt
@@ -45,7 +45,7 @@ class GardenActivity6Test {
         // When the "Add Plant" button is clicked
         onView(withId(R.id.add_plant)).perform(click())
 
-         Thread.sleep(60000)
+        Thread.sleep(60000)
 
         // Then the ViewPager should change to the Plant List page
         onView(withId(R.id.plant_list)).check(matches(isDisplayed()))
